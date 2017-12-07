84e6713525f9836d8d1fdd1bf0b8762db6a62ede
xuxiaoshuo <289539773@qq.com>
2017-12-07 23:19:59 +0800

    fix some bugs

--- model.sh
+++ model.sh
@@ -1,8 +1,1 @@
-./model you_19_2048/ you_19_2048/merge_data_test_19_2048/ you_list 100 32 9 3 you_19_2048/center 11 19
-./model you_19_2048/ you_19_2048/merge_data_test_19_2048/ you_list 100 32 9 4 you_19_2048/center 11 19
-
-./model you_19_2048/ you_19_2048/merge_data_test_19_2048/ you_list 100 32 8 5 you_19_2048/center 11 19
 ./model you_19_2048/ you_19_2048/merge_data_test_19_2048/ you_list 100 32 9 5 you_19_2048/center 11 19
-./model you_19_2048/ you_19_2048/merge_data_test_19_2048/ you_list 100 32 10 5 you_19_2048/center 11 19
-./model you_19_2048/ you_19_2048/merge_data_test_19_2048/ you_list 100 32 11 5 you_19_2048/center 11 19
-./model you_19_2048/ you_19_2048/merge_data_test_19_2048/ you_list 100 32 12 5 you_19_2048/center 11 19
